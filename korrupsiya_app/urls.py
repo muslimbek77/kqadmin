ce67f7c8df30955733c424b0c1ec82998644be71
muslimbek77 <muslimjonbekali@mail.ru>
2026-06-05 10:53:06 +0500

    fix slash problem

--- korrupsiya_app/urls.py
+++ korrupsiya_app/urls.py
@@ -13,8 +13,12 @@ urlpatterns = [
     path('korrupsiya-file/', KorrupsiyaFileListView.as_view(), name='korrupsiya-file-list'),
     path('vacancies/', VacancyListAPIView.as_view(), name='vacancy-list'),
     path('vacancies/<int:pk>/', VacancyDetailAPIView.as_view(), name='vacancy-detail'),
+    path('murojaatlar', MurojaatListCreateAPIView.as_view()),
     path('murojaatlar/', MurojaatListCreateAPIView.as_view(), name='murojaat-list-create'),
+    path('murojaatlar/statistics', MurojaatStatisticsAPIView.as_view()),
     path('murojaatlar/statistics/', MurojaatStatisticsAPIView.as_view(), name='murojaat-statistics'),
+    path('murojaatlar/<int:pk>', MurojaatDetailAPIView.as_view()),
     path('murojaatlar/<int:pk>/', MurojaatDetailAPIView.as_view(), name='murojaat-detail'),
+    path('murojaatlar/<int:pk>/status', MurojaatStatusUpdateAPIView.as_view()),
     path('murojaatlar/<int:pk>/status/', MurojaatStatusUpdateAPIView.as_view(), name='murojaat-status-update'),
 ]

dd931b3a3feba61353533f353fd361dc18578c04
muslimbek77 <muslimjonbekali@mail.ru>
2026-06-10 12:02:18 +0500

    fix

--- korrupsiya_app/urls.py
+++ korrupsiya_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     KorrupsiyaCreateView, KorrupsiyaUpdateView, KarrupsiyaMalumotListView, KorrupsiyaMalumotDetailView,
-    KorrupsiyaMalumotUpdateView, KorrupsiyaFileListView, KorrupsiyaFileUpdateView, VacancyListAPIView, VacancyDetailAPIView,
+    KorrupsiyaFileListView, VacancyListAPIView, VacancyDetailAPIView,
     MurojaatListCreateAPIView, MurojaatDetailAPIView, MurojaatStatusUpdateAPIView,
     MurojaatStatisticsAPIView, TelegramWebhookAPIView,
 )
@@ -11,9 +11,7 @@ urlpatterns = [
     path('korrupsiya/<int:pk>/', KorrupsiyaUpdateView.as_view(), name='korrupsiya-update'),
     path('karrupsiya-malumot/', KarrupsiyaMalumotListView.as_view(), name='karrupsiya-malumot-list'),
     path('karrupsiya-malumot/<int:pk>/', KorrupsiyaMalumotDetailView.as_view(), name='karrupsiya-malumot-detail'),
-    path('karrupsiya-malumot/<int:pk>/update/', KorrupsiyaMalumotUpdateView.as_view(), name='karrupsiya-malumot-update'),
     path('korrupsiya-file/', KorrupsiyaFileListView.as_view(), name='korrupsiya-file-list'),
-    path('korrupsiya-file/<int:pk>/update/', KorrupsiyaFileUpdateView.as_view(), name='korrupsiya-file-update'),
     path('vacancies/', VacancyListAPIView.as_view(), name='vacancy-list'),
     path('vacancies/<int:pk>/', VacancyDetailAPIView.as_view(), name='vacancy-detail'),
     path('murojaatlar', MurojaatListCreateAPIView.as_view()),
